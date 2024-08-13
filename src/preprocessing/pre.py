b5542c1f4ed79b9238866e8370e55f89d14a2846
Marcello Nascif <marcellobnf@gmail.com>
2024-08-12 23:22:02 -0300

    pre-processing done

--- src/preprocessing/pre.py
+++ src/preprocessing/pre.py
@@ -1,15 +1,12 @@
 import pandas as pd
 
 path = "data/"
-
 df = pd.read_csv(path + "Temporal Geração de Energia.csv", sep=';')
 
+df.drop(columns=["Cor Período 2 GE", "Data Escala de Tempo 2 GE"], inplace=True)
+df.rename(columns={"Data Dica Temp 2": "data", "Selecione Tipo de GE": "geracao"}, inplace=True)
 
+df.to_csv(path + "geracao_solar.csv", sep=';')
 
-# df[['month', 'year']] = df['month_year'].str.split(' de ', expand=True)
-
-# string_to_number = {'janeiro': '01', 'fevereiro': '02', 'março': '03', 'abril': '04', 'maio': '05', 'junho': '06', 'julho': '07', 'agosto': '08', 'setembro': '09', 'outubro': '10', 'novembro': '11', 'dezembro': '12'}
-# df['month'] = df['month'].replace(string_to_number)
-
-
-print(df.tail())
+print(df.info())
+print(df.tail(5))
