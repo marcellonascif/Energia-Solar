728a0f43e074c19b6870ad374a550090bd5ebe30
Marcello Nascif <marcellobnf@gmail.com>
2024-08-12 23:04:53 -0300

    estruturando o csv

---
+++ src/preprocessing/pre.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+path = "data/"
+
+df = pd.read_csv(path + "geracao_solar.csv", sep=';')
+
+
+
+# df[['month', 'year']] = df['month_year'].str.split(' de ', expand=True)
+
+# string_to_number = {'janeiro': '01', 'fevereiro': '02', 'março': '03', 'abril': '04', 'maio': '05', 'junho': '06', 'julho': '07', 'agosto': '08', 'setembro': '09', 'outubro': '10', 'novembro': '11', 'dezembro': '12'}
+# df['month'] = df['month'].replace(string_to_number)
+
+
+print(df[['month', 'year']].tail())
