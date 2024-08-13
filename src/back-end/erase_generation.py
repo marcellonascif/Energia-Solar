8d174218ebed3e7a123d621b07fdbfcbf3922cec
Marcello Nascif <marcellobnf@gmail.com>
2024-08-12 23:53:07 -0300

    funcao de apagar valores de geracao

--- src/back-end/erase_generation.py
+++ src/back-end/erase_generation.py
@@ -1,4 +1,18 @@
+import numpy as np
 import pandas as pd
+import random
+
+def erase(df: pd.DataFrame, column: str, iter: int=1):
+    '''
+    Function to erase some data from the DataFrame
+    '''
+
+    for i in range(iter):
+        line = random.randint(0, len(df) - 1)
+        df.loc[line, column] = np.nan
+
+    return df
+
 
 path = "data/"
 df = pd.read_csv(path + "geracao_solar.csv", sep=';')
@@ -6,6 +20,7 @@ df = pd.read_csv(path + "geracao_solar.csv", sep=';')
 df.rename(columns={"Unnamed: 0": "id"}, inplace=True)
 
 df["data"] = pd.to_datetime(df["data"], format="%Y-%m-%d")
-# df.set_index("data", inplace=True)
+
+df = erase(df, "geracao", 5)
 
 print(df.info())
